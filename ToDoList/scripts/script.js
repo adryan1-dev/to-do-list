f353ef656602a911192c6ed7bfbdd3151b290c3a
Adryan <130511954+adryan1-dev@users.noreply.github.com>
2024-03-10 17:23:06 -0300

    finalização do front-end do projeto

--- ToDoList/scripts/script.js
+++ ToDoList/scripts/script.js
@@ -17,9 +17,13 @@ const handleAddTask = () =>{
     const taskContent = document.createElement('p')
     taskContent.innerText = inputElement.value
 
+    taskContent.addEventListener('click', () => handleClick(taskContent))
+
     const deleteItem = document.createElement('i')
     deleteItem.classList.add('fa-solid')
     deleteItem.classList.add('fa-trash-can')
+
+    deleteItem.addEventListener('click', () => handleDeleteClick(taskItemContainer, taskContent))
 
     taskItemContainer.appendChild(taskContent)
     taskItemContainer.appendChild(deleteItem)
@@ -27,6 +31,33 @@ const handleAddTask = () =>{
     tasksContainer.appendChild(taskItemContainer)
 
     inputElement.value = ''
+
+    updateLocalStorage()
+}
+
+const handleClick = (taskContent) =>{
+    const tasks = tasksContainer.childNodes
+
+    for(const task of tasks){
+        if(task.firstChild.isSameNode(taskContent)){
+            task.firstChild.classList.toggle('completed')
+        }
+    }
+
+    updateLocalStorage()
+}
+
+const handleDeleteClick = (taskItemContainer, taskContent) =>{
+    const tasks = tasksContainer.childNodes
+
+    for(const task of tasks){
+        const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent)
+        if(currentTaskIsBeingClicked){
+            taskItemContainer.remove()
+        }
+    }
+
+    updateLocalStorage()
 }
 
 const handleInputChange = () =>{
